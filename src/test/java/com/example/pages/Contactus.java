1f57d831ba60d1f5d1737687f4f48bcae4b0b8e1
Patricamontano <johanna.patricia.montano@gmail.com>
2024-01-13 00:01:27 -0500

    Eliminar metodos redundantes

--- src/test/java/com/example/pages/Contactus.java
+++ src/test/java/com/example/pages/Contactus.java
@@ -28,29 +28,4 @@ public class Contactus {
         String result = "Su comentario ha sido enviado con éxito al propietario de la tienda.";
         return result.equalsIgnoreCase(element.getText());
     }
-
-
-
-
-
-    public void fillFormCheckout(String name,String email,String socialNumber,String phone){
-        this.driver.findElement(By.xpath("//span[.='Name']//preceding-sibling::input[1]")).sendKeys(name);
-        this.driver.findElement(By.xpath("//span[.='Email Address']//preceding-sibling::input[1]")).sendKeys(email);
-        this.driver.findElement(By.xpath("//span[.='Social Security Number']//preceding-sibling::input[1]")).sendKeys(socialNumber);
-        this.driver.findElement(By.xpath("//span[.='Phone Number']//preceding-sibling::input[1]")).sendKeys(phone);
-    }
-
-    public void uploadPhoto(String pathPhoto){
-        this.driver.findElement(By.cssSelector("input[type='file']")).sendKeys(pathPhoto);
-    }
-
-    public void addPromotion(String codPromotion){
-        this.driver.findElement(By.name("promo")).sendKeys(codPromotion);
-        this.driver.findElement(By.xpath("//button[.='Apply']")).click();
-    }
-
-    public void selectPayNow(){
-        this.driver.findElement(By.cssSelector("[data-react-toolbox='check']")).click();
-        this.driver.findElement(By.xpath("//button[.='Pay now']")).click();
-    }
 }
